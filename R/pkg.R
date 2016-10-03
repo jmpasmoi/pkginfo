233fa734d6df72f68d9f4258e5a53a69c6940f83
Jean Marie Cimula <jmkacim@gmail.com>
2016-10-03 14:05:17 +0900

    pkginfo

--- R/pkg.R
+++ R/pkg.R
@@ -2,7 +2,7 @@
 #'
 #' @export
 #'
-pkginfo <- function(){
+pkgs <- function(){
 
   pkg_path <- .libPaths()
   allpaths <- pkg_stat(pkg_path)$rlib
@@ -26,7 +26,7 @@ pkginfo <- function(){
 
 pkg_size <- function(path){
 
-  setwd(dir = path)
+  setwd(dir=path)
   total <- round(
              sum(
                   file.info(
@@ -51,3 +51,55 @@ pkg_stat <- function(path){
   invisible(rpath)
 }
 
+#' Tools to get interactively information of R packages
+#'
+#' @export
+#'
+pkginfo <- function(pkgname){
+
+  pkg_path <- .libPaths()
+  allpaths <- pkg_stat(pkg_path)$rlib
+  cmp <- FALSE
+
+  for(i in 1:length(allpaths)){
+
+    m_path <- allpaths[i]
+    m_dir <- dir(m_path)
+
+    for(j in 1:length(m_dir)){
+
+      cur_dir <- m_dir[j]
+      each_dir <- trimws(paste0(paste0(m_path,"/"), file.path(cur_dir)))
+
+      if(dir.exists(each_dir) && identical(cur_dir,pkgname)){
+
+        cmp <- TRUE
+        break;
+
+      }else{ next}
+
+    }
+  }
+
+  if(cmp){
+
+     size_mb <- pkg_size(each_dir)
+     dps <- desc::description$new(each_dir)
+     cit <-  citation(package=pkgname)[[1]]$title
+
+
+  }else{
+    stop(paste0("There is no package called: ",pkgname), call.=FALSE)
+  }
+
+  pkg <- list(
+                pkgname=pkgname,
+                version=packageVersion(pkgname),
+                pkgsize=paste0(size_mb,"Mb"),
+                title=cit,
+                maintenair=dps$get_maintainer(),
+                depends=dps$get_deps()
+              )
+
+  return(pkg)
+}
